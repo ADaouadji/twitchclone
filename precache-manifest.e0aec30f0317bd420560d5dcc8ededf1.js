8857a8395b0ff2ff51cdb9c814c64b1e1d18bfb5
ADaouadji <daouadji.a@hotmail.fr>
2020-12-21 13:11:57 +0100

    Updates

---
+++ precache-manifest.e0aec30f0317bd420560d5dcc8ededf1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a31bada2c076f2f27cdfd8fb143520",
+    "url": "/twitchclone/index.html"
+  },
+  {
+    "revision": "82f89300b2175a3f8bac",
+    "url": "/twitchclone/static/css/main.f3866dbc.chunk.css"
+  },
+  {
+    "revision": "5eb92105a5f59ed9f0b4",
+    "url": "/twitchclone/static/js/2.a6a4a823.chunk.js"
+  },
+  {
+    "revision": "82f89300b2175a3f8bac",
+    "url": "/twitchclone/static/js/main.43a0dfb5.chunk.js"
+  },
+  {
+    "revision": "107f8f8e1ffa7a5dbc6f",
+    "url": "/twitchclone/static/js/runtime-main.1a8e8922.js"
+  },
+  {
+    "revision": "a55a5aa33744d9d0c759d774f4fea92a",
+    "url": "/twitchclone/static/media/Croix.a55a5aa3.svg"
+  },
+  {
+    "revision": "bea3cace01fd1bb7cce02538f2f1e9ad",
+    "url": "/twitchclone/static/media/IconeTwitch.bea3cace.svg"
+  },
+  {
+    "revision": "29c57c4d128e946c27b22138c5ddba5b",
+    "url": "/twitchclone/static/media/MenuIco.29c57c4d.svg"
+  },
+  {
+    "revision": "862e4fe3b1103d98eab47fc95a10f678",
+    "url": "/twitchclone/static/media/Search.862e4fe3.svg"
+  }
+]);
